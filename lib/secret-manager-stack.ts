e7f7931f22dcb5c3557852d2b9c63ec5fa826ab4
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2022-12-26 18:32:44 -0300

    Fixed the code

--- lib/secret-manager-stack.ts
+++ lib/secret-manager-stack.ts
@@ -3,36 +3,30 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
 import * as AWS from 'aws-sdk';
+import { Construct } from 'constructs'
 
-interface Secret {
-  parameter: string;
-  secret: string;
+interface secretsObject {
+  secrets : string[][];
 }
 
 export class SecretManagerStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Read the secrets from the YAML file
-    const secrets = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, 'secrets.yml'), 'utf8'));
+    const YAMLfile = yaml.load(fs.readFileSync(path.resolve(__dirname, 'secrets.yml'), 'utf8')) as secretsObject;
 
-    // Create a new AWS.SSM client
     const ssm = new AWS.SSM();
 
-    // Create a new AWS.SecretsManager client
     const secretsManager = new AWS.SecretsManager();
 
-    // Iterate over each secret in the YAML file
-    secrets.secrets.forEach(async (secret) => {
+    YAMLfile["secrets"].forEach(async (secret) => {
 
-      console.log(`Parameter Store - Existing parameter name : ${secret.parameter}`)
-      console.log(`Secret Manager - New secret name          : ${secret.secret}`)
+      console.log(`Parameter Store - Existing parameter name : ${secret[0]}`)
+      console.log(`Secret Manager  - New secret name         : ${secret[1]}`)
 
-      // Get the value of the parameter from AWS Parameter Store
-      const value = await this.getParameterValue(ssm, secret.parameter);
+      const value = await this.getParameterValue(ssm, secret[0]);
 
-      // Create a new secret in AWS Secrets Manager with the same name as the parameter and the value from Parameter Store
-      await this.createSecret(secretsManager, secret.secret, value);
+      await this.createSecret(secretsManager, secret[1], value);
     });
   }
 
@@ -41,7 +35,7 @@ export class SecretManagerStack extends cdk.Stack {
       const result = await ssm.getParameter({ Name: parameter, WithDecryption: true }).promise();
 
       if (result.Parameter) {
-        return result.Parameter.Value;
+        return result.Parameter.Value!;
       } else {
         throw new Error(`Parameter not found: ${parameter}`);
       }
