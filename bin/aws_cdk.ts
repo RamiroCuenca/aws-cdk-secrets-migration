ea8a3ebf14dd46b0e482e85da3db99050ba3d860
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2023-01-03 13:19:00 -0300

    Created a test sample of the stack

--- bin/aws_cdk.ts
+++ bin/aws_cdk.ts
@@ -4,3 +4,11 @@ import { SecretManagerStack } from '../lib/secret-manager-stack';
 
 const app = new cdk.App();
 new SecretManagerStack(app, 'SecretManagerStack');
+
+// Create a new stack of SecretManagerStack
+const testStack = new SecretManagerStack(app, 'AnotherSecretManagerStack', {
+  env: {
+    account: 'ACCOUNT_ID', // AWS account ID,
+    region: 'us-east-1' // Region where the stack will be created
+  }
+});
